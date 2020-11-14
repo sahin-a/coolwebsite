6d8f0cc2354e859e7db114038921959f65183852
sar <sar@NB-SAR-WIN10>
2020-11-14 19:17:09 +0100

    redditripper.php added

---
+++ src/redditripper.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../public/css/bootstrap.css">
+    <title>Login Page</title>
+</head>
+<body class="bg-dark align-self-center m-2 p-2">
+<div class="container">
+    <div class="input-group">
+        <!-- Top Row [Subreddit -->
+    </div>
+</div>
+</body>
+</html>
